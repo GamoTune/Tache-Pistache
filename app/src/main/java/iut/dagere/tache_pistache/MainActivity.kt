9072baeffb77c6a7132f2e262ac91cf5aeaa3c42
Dabert Sébastien <sebdab86@gmail.com>
2026-02-24 13:45:32 +0100

    Add initial project structure and configuration files for Tache-pistache app

---
+++ app/src/main/java/iut/dagere/tache_pistache/MainActivity.kt
@@ -0,0 +1,47 @@
+package iut.dagere.tache_pistache
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import iut.dagere.tache_pistache.ui.theme.TachepistacheTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            TachepistacheTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TachepistacheTheme {
+        Greeting("Android")
+    }
+}
