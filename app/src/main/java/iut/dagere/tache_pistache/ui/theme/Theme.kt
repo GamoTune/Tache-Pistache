b4c3fc8080905f8ceb3245953188e9ac844fbc68
Arthur Labregere <arthur.labregere@etu.unilim.fr>
2026-02-24 13:47:49 +0100

    Refactor project structure and update theme naming conventions

--- app/src/main/java/iut/dagere/tache_pistache/ui/theme/Theme.kt
+++ app/src/main/java/iut/dagere/tache_pistache/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun TachepistacheTheme(
+fun TachePistacheTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
